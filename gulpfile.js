c58386f2c82741c6781fe4cd01f79ee5b77b45fd
azure-architect <vscoder.brad@gmail.com>
2024-01-24 16:43:50 +0800

    fixed issue with gulp not running an update to css on save. only refreshing the browser.

--- gulpfile.js
+++ gulpfile.js
@@ -9,30 +9,31 @@ const rename = require('gulp-rename');
 
 // CSS processing task
 gulp.task('css', () => {
-  return gulp.src('src/css/style.css') // Ensure this is the entry point file for your CSS
-    .pipe(sourcemaps.init())
-    .pipe(postcss([
-      tailwindcss, // Add Tailwind CSS as a PostCSS plugin
-      autoprefixer(),
-      postcssNested
-      // Add other PostCSS plugins here if needed
-    ]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(rename('prod.css')) // Renaming the output file
-    .pipe(gulp.dest('dist/css')) // Output directory
-    .pipe(browserSync.stream());
+    return gulp.src('src/css/style.css') // Ensure this is the entry point file for your CSS
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            tailwindcss, // Add Tailwind CSS as a PostCSS plugin
+            autoprefixer(),
+            postcssNested
+            // Add other PostCSS plugins here if needed
+        ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename('prod.css')) // Renaming the output file
+        .pipe(gulp.dest('dist/css')) // Output directory
+        .pipe(browserSync.stream());
 });
 
 // Watch task with BrowserSync
 gulp.task('watch', () => {
-  browserSync.init({
-    proxy: "http://timber.local",
-    notify: true
-  });
+    browserSync.init({
+        proxy: "http://timber.local",
+        notify: true
+    });
 
-  gulp.watch('src/css/**/*.css', gulp.series('css'));
-  gulp.watch('./views/**/*.twig').on('change', browserSync.reload);
-  gulp.watch('./*.php').on('change', browserSync.reload);
+    // Watch for changes in CSS and Twig files
+    gulp.watch('src/css/**/*.css', gulp.series('css'));
+    gulp.watch('./views/**/*.twig').on('change', gulp.series('css', browserSync.reload));
+    gulp.watch('./*.php').on('change', gulp.series('css', browserSync.reload));
 });
 
 // Default task
